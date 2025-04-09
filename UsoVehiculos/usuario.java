fdac4f170b1fc2d51a9e32ca77bff2c8f07a831f
DanielTirados <daniel.tirado.izq@gmail.com>
2025-04-08 20:59:05 -0400

    read me impl,main impl, usuario class impl,vehiculos class imp.

---
+++ UsoVehiculos/usuario.java
@@ -0,0 +1,45 @@
+package UsoVehiculos;
+
+public class usuario {
+    private int numerodeestudinte;
+    private String name;
+    private String email;
+    private String telefono;
+
+    //contructor
+
+    usuario(int numerodeestudinte, String name, String email, String telefono){
+        this.numerodeestudinte=numerodeestudinte;
+        this.name=name;
+        this.email=email;
+        this.telefono=telefono;
+    }
+
+    //getters
+
+    public int getNumeroDeEstudiante(){
+    return this.numerodeestudinte;
+    }
+    public String getName(){
+    return this.name;
+    }
+    public String getEmail(){
+    return this.email;
+    }
+    public String getTelefono(){
+    return this.telefono;
+    }
+    //setters
+    public void setNumeroDeEstudiante(int id){
+        this.numerodeestudinte=id; 
+    }
+    public void setName(String name){
+        this.name=name; 
+    }
+    public void setEmail(String email){
+        this.email=email; 
+    }
+    public void setTelefono(String telefono){
+        this.telefono=telefono; 
+    }
+}
