d43dc8457127117738931befede203c59e6e7641
DanielTirados <daniel.tirado.izq@gmail.com>
2025-04-10 19:06:17 -0400

    agregar method

--- UsoVehiculos/main.java
+++ UsoVehiculos/main.java
@@ -12,17 +12,34 @@ public class main {
      System.out.println(" por favor elija una opcion: ");
      System.out.println("1. Agregar o Eliminar Vehiculos");
      System.out.println("2. Agregar o Eliminar Usuarios");
-     System.out.println("3. Agregar o Elinar Reservaciones");
-     System.out.println("4. Agregar o eliminar Usuarios en la Lista de Espera");
+     System.out.println("3. Agregar o Eliminar Reservaciones");
+     System.out.println("4. Agregar o Eliminar Usuarios en la Lista de Espera");
      System.out.println("5. Mostrar Reservaciones");
      System.out.println("6. Mostrar Usuarios");
      System.out.println("7. Mostrar Vehiculos");
      System.out.println("8. Mostrar Lista de Espera");
      System.out.println("9. Modificaciones");
      System.out.println("10. Finalizar");
-     System.out.println("Seleccionado:");
      press= sc.nextInt();
     
+     if(press==1){
+      int press2=0; 
+        System.out.println("1. Agregar vehiculo");
+        System.out.println("2. Eliminar vehiculo");
+        press2= sc.nextInt();
+         if(press2==1){
+            Vehiculos.Agregar();
+         }
+         else if(press2==2){
+            Vehiculos.Eliminar();
+         }
+         else{
+            System.out.println("Opcion no valida, por favor intente de nuevo.");
+         }
+     }
+     if(press==7){
+        Vehiculos.Mostrar();
+     }
      if (press==10){
         finalizado=true;
         System.out.println("Gracias por usar nuestro servicio, hasta luego.");
--- UsoVehiculos/vehiculos.java
+++ UsoVehiculos/vehiculos.java
@@ -1,10 +1,14 @@
 package UsoVehiculos;
 
+import java.util.Scanner;
+import java.util.HashSet;
 public class Vehiculos {
 
     private int id;
     private String vehiculo;
     private Estacion lugar;
+    static HashSet<Vehiculos> vehiculos= new HashSet<>();
+    static Scanner sc=new Scanner(System.in);
 
 //contructor
 Vehiculos(int id,String vehiculo,Estacion lugar){
@@ -39,6 +43,43 @@ public void setVehiculos(String vehiculos){
 public void setLugar(Estacion lugar){
     this.lugar=lugar;
 }
+ 
 //methods
+public static void Agregar() {
+    System.out.println("¿Cuántos vehículos desea agregar?");
+    int cantidad = sc.nextInt();
+    for (int i = 0; i < cantidad; i++) {
+        System.out.println("Ingrese el ID del vehículo: ");
+        int id = sc.nextInt();
+        System.out.println("Ingrese el nombre del vehículo: ");
+        String vehiculo = sc.next();
+        System.out.println("Ingrese la estación del vehículo: ");
+        String estacion = sc.next();
+        Estacion lugar = new Estacion(estacion, 10);
+        Vehiculos vehiculo1 = new Vehiculos(id, vehiculo, lugar);
+        if (vehiculos.add(vehiculo1)) {
+            System.out.println("Vehículo agregado con éxito.");
+        } else {
+            System.out.println("El vehículo con ID " + id + " ya existe y no se puede agregar.");
+        }
+    }
+}
 
+public static void Eliminar(){
+    System.out.println("Ingrese el ID del vehiculo a eliminar: ");
+    int id= sc.nextInt();
+    for (Vehiculos vehiculo: vehiculos){
+        if (vehiculo.getId()==id){
+            vehiculos.remove(vehiculo);
+            System.out.println("Vehiculo eliminado con exito");
+            return;
+        }
+    }
+    System.out.println("Vehiculo no encontrado");
+}
+public static void Mostrar(){
+    for (Vehiculos vehiculo: vehiculos){
+        System.out.println("ID: "+vehiculo.getId()+" Vehiculo: "+vehiculo.getVehiculo()+" Estacion: "+vehiculo.getLugar().getUbicacion());
+    }
+}
 }
