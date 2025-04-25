b8de8cde92680f1e5bd89d50264692b21f6201ae
DanielTirados <daniel.tirado.izq@gmail.com>
2025-04-25 09:53:51 -0400

    ui improvement

--- UsoVehiculos/main.java
+++ UsoVehiculos/main.java
@@ -22,13 +22,12 @@ public class main {
             System.out.println("1. Agregar o Eliminar Vehículos");
             System.out.println("2. Agregar o Eliminar Usuarios");
             System.out.println("3. Agregar o Eliminar Reservaciones");
-            System.out.println("4. Agregar o Eliminar Usuarios en la Lista de Espera");
-            System.out.println("5. Mostrar Reservaciones");
-            System.out.println("6. Mostrar Usuarios");
-            System.out.println("7. Mostrar Vehículos");
-            System.out.println("8. Mostrar Lista de Espera");
-            System.out.println("9. Modificaciones");
-            System.out.println("10. Finalizar");
+            System.out.println("4. Mostrar Reservaciones");
+            System.out.println("5. Mostrar Usuarios");
+            System.out.println("6. Mostrar Vehículos");
+            System.out.println("7. Mostrar Lista de Espera");
+            System.out.println("8. Modificaciones");
+            System.out.println("9. Finalizar");
             System.out.println("--------------------------------------");
             System.out.print("Ingrese una opción: ");
             press = sc.nextInt();
@@ -76,33 +75,31 @@ public class main {
                     else System.out.println("Opción no válida. Intente de nuevo.");
                     break;
 
-                case 5:
+                case 4:
                     System.out.println("\nMostrando Reservaciones");
                     Reservaciones.mostrarTodasLasReservaciones();
                     break;
-                case 6:
+                case 5:
                     System.out.println("\nMostrando Usuarios");
                     Usuario.MostrarUsuario();
                     break;    
 
-                case 7:
+                case 6:
                     System.out.println("\nMostrando Vehículos");
                     Vehiculos.Mostrar();
                     break;
-                case 9:
+                case 8:
                 System.out.println("\nGestion de Modificaciones");
                 System.out.println("--------------------------------------");
                 System.out.println("1. Modificar Usuario");
                 System.out.println("2. Modificar Vehículo");
-                System.out.println("3. Modificar Reservacion");              
-                System.out.println("4. Modificar Lista de Espera");
                 System.out.println("--------------------------------------");
                 System.out.print("Seleccione una opción: ");
                 int opcionModificacion = sc.nextInt();
                 if(opcionModificacion==1){Usuario.ModificaciondeUsuario();}
                 else if(opcionModificacion==2){Vehiculos.ModificaciondeVehiculo();}
                     break;
-                case 10:
+                case 9:
                 System.out.println("Las reservaciones de hoy son:");
                 Reservaciones.mostrarTodasLasReservaciones();
                 System.out.println("\nGracias por usar nuestro servicio. Hasta luego.");
